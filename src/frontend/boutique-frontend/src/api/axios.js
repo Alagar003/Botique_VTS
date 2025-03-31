10cb77f0295eb89882a11123f3c63c229ef66629
Alagar003 <azhagumari003@gmail.com>
2025-03-31 12:47:28 +0530

    Change Cors

--- src/frontend/boutique-frontend/src/api/axios.js
+++ src/frontend/boutique-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8081', // Adjust according to your backend
+    baseURL: 'https://alagar003.github.io', // Adjust according to your backend
 });
 
 export default API;
